ec0d5aba98be2df28db05d19705501c7bdfead05
Greg Ziegan <greg.ziegan@gmail.com>
2015-11-02 13:10:42 -0500

    implement plot creator

---
+++ Anovelmous/components/PlotCreator.js
@@ -0,0 +1,40 @@
+import React, {
+  TextField,
+  View,
+} from 'react-native';
+
+export default PlotCreator = React.createClass({
+  getInitialState: function() {
+    return { summary: '' };
+  },
+
+  _resetFormData: function() {
+    this.setState({ summary: '' });
+  },
+
+  _handleSummaryChange: function(event) {
+    this.setState({ summary: event.target.value });
+  },
+
+  _handleOnSubmit: function(event) {
+    event.preventDefault();
+    const { summary } = this.state;
+    this.props.onCreate(summary);
+    this._resetFormData();
+  },
+
+  render() {
+    const { summary } = this.state;
+    return (
+      <View>
+        <TextField
+          placeholder="Enter a brief summary of the novel's plot"
+          value={summary}
+          onChangeText={this._handleSummaryChange}
+          />
+        <TouchableOpacity
+          onPress={this._handleOnSubmit} />
+      </View>
+    );
+  }
+});
